99a8c0e2e3d1606541cbd273d55de2f4e6f923a4
Marvs2 <marvinrvillanueva@iskolarngbayan.edu.ph>
2024-09-15 17:35:39 +0800

    with simple connection table -delete of details

--- nodemysql/index.js
+++ nodemysql/index.js
@@ -35,6 +35,53 @@ app.get('/createdb', (req, res) => {
     });
 });
 
+// create table in database
+app.get('/createemployee', (req, res) => {
+    let sql = 'CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(255), designation VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql, (err) => {
+        if(err){
+            throw err
+        }
+        res.send('Employee table created');
+    })
+})
+
+//Insert a new employee first 
+app.get('/employee1', (req, res) => {
+    let post = {name: 'Jake Smith', designation: 'Chief Executive Officer'}
+    let sql = 'INSERT INTO employee SET ?'
+    let query = db.query(sql, post ,err => {
+        if(err){
+            throw err
+        }
+        res.send('Employee table created');
+    })
+})
+
+//fetch all the employees from the database
+app.get('/getemployee', (req, res) => {
+    let sql = 'SELECT * FROM employee'
+    let query = db.query(sql, (err, results) => {
+        if(err){
+            throw err
+        }
+        console.log(results)
+        res.send('Employee details fetched');
+    })
+})
+
+
+//update the details of emplyee based on Id
+app.get('/updateemployee/:id',(req, res) => {
+    let newName = 'Updated name'
+    let sql = `UPDATE employee SET name = '${newName}' WHERE id = ${req.params.id}`
+    let query = db.query(sql, err => {
+        if (err){
+            throw err
+        }
+        req.send('Employee details updated successfully');
+    })
+})
 
 app.listen('3000', () => {
         console.log("server started on port 3000");
